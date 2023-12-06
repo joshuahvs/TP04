fb464156b5a5ed91259b39caa80bcf50b142455e
vito1012buss <thebestvitodiaries@gmail.com>
2023-12-06 23:36:31 +0700

    duh

--- TP04.py
+++ TP04.py
@@ -49,6 +49,51 @@ class BarcodeGenerator:
     def create_barcode(self):
         self.canvas.create_rectangle(50, 50, 150, 150, outline="black", fill="white")
 
+
+class EAN_13:
+    structure = {0: "LLLLLL RRRRRR",
+                 1: "LLGLGG RRRRRR",
+                 2: "LLGGLG RRRRRR",
+                 3: "LLGGGL RRRRRR",
+                 4: "LGLLGG RRRRRR",
+                 5: "LGGLLG RRRRRR",
+                 6: "LGGGLL RRRRRR",
+                 7: "LGLGLG RRRRRR",
+                 8: "LGLGGL RRRRRR",
+                 9: "LGGLGL RRRRRR"}
+    l_code = {0: "0001101",
+              1: "0011001",
+              2: "0010011",
+              3: "0111101",
+              4: "0100011",
+              5: "0110001",
+              6: "0101111",
+              7: "0111011",
+              8: "0110111",
+              9: "0001011"}
+    g_code = {0: "0100111",
+              1: "0110011",
+              2: "0011011",
+              3: "0100001",
+              4: "0011101",
+              5: "0111001",
+              6: "0000101",
+              7: "0010001",
+              8: "0001001",
+              9: "0010111"}
+    r_code = {0: "1110010",
+              1: "1100110",
+              2: "1101100",
+              3: "1000010",
+              4: "1011100",
+              5: "1001110",
+              6: "1010000",
+              7: "1000100",
+              8: "1001000",
+              9: "1110100"}
+
+
+
 def main():
     root = Tk()
     BarcodeGenerator(root)
