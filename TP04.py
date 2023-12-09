19c318c29121bbd01304eddb602dd694f3714e6b
vito1012buss <thebestvitodiaries@gmail.com>
2023-12-09 17:24:02 +0700

    adding comment, i think it's done??no??

--- TP04.py
+++ TP04.py
@@ -1,42 +1,44 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox # import tkinter dan juga messagebox
 
+# Class untuk tampilan awal
 class Display:
+    # Inisiasi awal dan mengatur title window dan geometry
     def __init__(self, master):
         self.master = master
-
         master.title("EAN-13")
         master.geometry("400x500")
-        # master.resizable(False, False)
         self.homepage()
 
+    # Fungsi yang akan menampilkan display awal
     def homepage(self):
+        # Label ps file
         ps_file_label = Label(self.master, text="Save Barcode to PS file [eg: EAN13.eps]:")
         ps_file_label.pack()
-
+        # Entry untuk memasukkan nama file
         self.ps_file_entry = Entry(self.master)
         self.ps_file_entry.pack()
-
+        # Label untuk angka
         number_label = Label(self.master, text="Enter code (first 12 decimal digits):")
         number_label.pack()
-
+        # Fungsi untuk memasukkan angka dan jika ditekan enter, maka barcode akan tergenerate
         self.number_entry = Entry(self.master)
+        self.number_entry.bind("<Return>", self.check_input_file)
         self.number_entry.pack()
-
-        self.button = Button(self.master, text= "CLICK ME", command= self.check_input_file)
-        self.button.pack()    
-
+        # Canvas untuk menggambarkan barcodenya
         self.canvas = Canvas(self.master, height="400", width="500", bg="white")
         self.canvas.pack()
 
+
+# Class untuk memproses dan menampilakan barcode
 class Barcode(Display):
+    # Inisiasi awal
     def __init__(self, master):
         super().__init__(master)
-        # self.number_entry.bind("<Return>", self.check_input_file)
-
-    def check_input_file(self):
+    # Fungsi yang akan menvalidasi input file
+    def check_input_file(self, event):
         file_name = self.ps_file_entry.get()
-        if file_name[-3:] != "eps":
+        if file_name[-4:] != ".eps":
             messagebox.showerror('Wrong input!', 'Please enter a valid postscript file')
         try:
             if open(file_name, 'r'):
@@ -45,7 +47,7 @@ class Barcode(Display):
             self.check_input_number()
             self.canvas.update()
             self.canvas.postscript(file = file_name, colormode = "color")
-
+    # Fungsi yang akan memvalidasi input angka
     def check_input_number(self):
         try:
             num = int(self.number_entry.get())
@@ -55,35 +57,33 @@ class Barcode(Display):
         if num_length != 12:
             messagebox.showerror('Wrong input!', 'The input must be a 12 digit number')
         self.check_digit(num)
-
+    # Fungis untuk check digit
     def check_digit(self, number):
-        # konversi digit jadi list str
-        digits = [int(digit) for digit in str(number)]
-        # Initialize checksum
+        digits = [int(digit) for digit in str(number)] # konversi digit jadi list str
         checksum = 0
-        # Iterate over each digit in the barcode
+        # Mengiterasi setiap digit
         for i, digit in enumerate(digits[::-1]):
-            # Multiply digit by 3 if its position is odd, otherwise by 1
-            weight = 3 if i % 2 == 0 else 1
-            # Add the weighted digit to the checksum
-            checksum += digit * weight
-        # Calculate the check digit
+            weight = 3 if i % 2 == 0 else 1 # Menghitung weightnya berdasarkan ganjil genapnya
+            checksum += digit * weight  # Menambahkan hasil perkalian ke checksum
+        # Menghitung check digitnya
         check_digit = (10 - (checksum % 10)) % 10
         full_number = str(number) + str(check_digit)
+        # Memanggil fungsi berikutnya
         self.display_check_digit(check_digit)
         self.display()
         self.bits(full_number)
     
-
+    # Fungsi yang menampilkan Check digitnya
     def  display_check_digit(self, check_digit):
         self.canvas.delete('checkdigit')
         self.canvas.create_text(200, 340, fill='orange',text=f'Check Digit: {check_digit}', font='Helvetica 24 bold', tag="checkdigit")
-
+    # Fungsi yang menampilkan untuk menampilkan teks "EAN-13 Barcode:"
     def display(self):
         self.canvas.delete('textawal')
         self.canvas.create_text(200, 35, text="EAN-13 Barcode: ", fill='black', font='Helvetica 24 bold', tag = 'textawal')
-
+    # Fungsi untuk encoding angka yang diinput user menjadi bits
     def bits(self, number):
+        #Dictionary struktur, untuk proses encodingnya
         structure = {"0": "LLLLLLRRRRRR",
                 "1": "LLGLGGRRRRRR",
                 "2": "LLGGLGRRRRRR",
@@ -127,10 +127,11 @@ class Barcode(Display):
         start_bits = "101"
         middle_bits = "01010"
         end_bits = "101"
+
+        # membuat angka yang diinput menjadi bits yang sesuai dengan ketentuan
         bits = ''
         first_digit = str(number)[0]
         rest_digit = str(number)[1:]
-
         structure_of_digit = structure[first_digit]
         for index, stc in enumerate(structure_of_digit):
             number_to_translate = rest_digit[index]
@@ -140,38 +141,38 @@ class Barcode(Display):
                 bits += g_code[number_to_translate]
             elif stc == "R":
                 bits += r_code[number_to_translate]
-
+        # Membagi bits untuk depan dan belakang
         first_6_bits = bits[:42]
         last_6_bits = bits[42:]
         self.make_barcode(start_bits, first_6_bits, middle_bits, last_6_bits, end_bits, number)
 
+    # Fungsi yang akan membuat dan menampilakan barcodenya
     def make_barcode(self, start, first, middle, last, end, number):
-
+        # Membagi angka
         first_digit = number[0]
         first_6 = str(number[1:7])
         last_6 =  number[7:]
-        print(first_digit)
-        print(first_6)
-        print(last_6)
-
+        # Mengukur letak barcode agar sesuai dengan canvas
         self.canvas.delete('barcode')
         canvas_width = 280
         canvas_height = 350
-        x_position = canvas_width * 0.25  # Start at 10% of canvas width
-        y_start = canvas_height * 0.2  # Start at 20% of canvas height
-        y_end = canvas_height * 0.8  # End at 80% of canvas height
+        x_position = canvas_width * 0.25  # posisi mulai 25% dari width canvas
+        y_start = canvas_height * 0.2  # Posisi mulai 20% dari height canvas
+        y_end = canvas_height * 0.8  # Posisi berhenti 80% of dari tinggi canvas
 
-        x_position_digit = canvas_width * 0.27
-
+        x_position_digit = canvas_width * 0.27 # Posisi khusus untuk angkanya
+        
         self.canvas.create_text(x_position - 8, 295, text=first_digit, fill='black', font='Helvetica 24 bold', tag = 'barcode')
+        # Mengiterasi untuk bit bagian start
         for bit in start:
             bit = int(bit)
             if bit == 1:
-                # Create a line if the bit is 1
+                # Menggambar barcodenya jika bit adalah 1 
                 self.canvas.create_rectangle(x_position, y_start, x_position + (canvas_width * 0.01), y_end, fill='blue', outline='blue', width=0, tag='barcode')
-            # Move to the next x position regardless of the bit value
-            x_position += canvas_width * 0.01  # Move by 1% of canvas width
+            # Menambah posisi x agar berpindah kesamping
+            x_position += canvas_width * 0.01  
 
+        # Mengiterasi untuk bit dari 6 angka pertama
         counter = 0
         index = 0
         for bit in first:
@@ -179,6 +180,7 @@ class Barcode(Display):
             counter += 1
             if bit == 1:
                 self.canvas.create_rectangle(x_position, y_start, x_position + (canvas_width * 0.01), canvas_height*0.75, fill='black', outline='black', width=0, tag='barcode')
+            # Jika sudah mengiterasi 7 bit, maka akan menampilkan angka (karena 1 angka di ubah menjadi 7 bit)
             if counter % 7 == 0:
                 try:
                     self.canvas.create_text(x_position_digit, 295, text=first_6[index], fill='black', font='Helvetica 24 bold', tag = 'barcode')
@@ -187,13 +189,14 @@ class Barcode(Display):
                     pass
             x_position += canvas_width * 0.01 
             x_position_digit += canvas_width * 0.01
-
+        # Mengiterasi untuk bagian tengah
         for bit in middle:
             bit = int(bit)
             if bit == 1:
                 self.canvas.create_rectangle(x_position, y_start, x_position + (canvas_width * 0.01), y_end, fill='blue', outline='blue', width=0, tag='barcode')
             x_position += canvas_width * 0.01 
         
+        # Mengiterasi untuk bagian bit dari 6 digit terakhir
         counter = 0
         index = 0
         x_position_digit += 10
@@ -211,16 +214,14 @@ class Barcode(Display):
             x_position += canvas_width * 0.01 
             x_position_digit += canvas_width * 0.01
         
+        # Mengiterasi untuk bagian paling akhir
         for bit in end:
             bit = int(bit)
             if bit == 1:
                 self.canvas.create_rectangle(x_position, y_start, x_position + (canvas_width * 0.01), y_end, fill='blue', outline='blue', width=0, tag='barcode')
             x_position += canvas_width * 0.01 
-        
-        # formatted_number = f"{number[:1]}  {number[1:7]}  {number[7:13]}"
-        # self.canvas.create_text(canvas_width / 2, canvas_height * 0.9, text=formatted_number, font=("Helvetica", 12), fill="black")
 
-
+# Menjalankan programnya dan membuat windownya tidak resizable
 def main():
     root = Tk()
     root.resizable(False,False)
